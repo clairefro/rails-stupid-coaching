d861c7c2f4a5bfd197648a78b89020491e05a4eb
claire froelich <claire.froelich@gmail.com>
2019-11-11 13:48:54 -0500

    finished challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,23 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
   def answer
+    @user_question = params[:question]
+
+    # coach response logic
+    if @user_question == "I am going to work"
+      @coach_response = "Great!"
+    elsif @user_question[-1] == '?'
+      @coach_response = "Silly question, I don't care get dressed and go to work!"
+    else
+      @coach_response = "I don't care, get dressed and go to work!"
+    end
+
+
+
+    # I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise she/he will answer I don't care, get dressed and go to work!
   end
 end
